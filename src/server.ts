80c1472c8c5a690fa852c9e7e865c1f0425b55ab
xx745 <xx745>
2023-04-09 20:28:46 +0100

    updated helmetjs to working version

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,13 @@ config();
 
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
-// import helmet from 'helmet'; // wrong types https://github.com/helmetjs/helmet/pull/405
+import helmet from 'helmet';
 
 export const app: Express = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(helmet());
+app.use(helmet());
 app.use(cors());
 
 app.get('/', (_req: Request, res: Response) => {
